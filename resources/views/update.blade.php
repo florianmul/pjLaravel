a2649fa7daf0d0971e27cedd57d238b8a91e7eea
florianmuller <jawamad@gmail.com>
2018-06-22 16:11:47 +0200

    front update

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -3,23 +3,28 @@
 @section('content')
     <div class="container bg-white">
 
-                {!! Form::open(['url'=>'/modify'])!!}
+                {!! Form::open(['url'=>'/modify','class' => 'form'])!!}
                 {!! Form::hidden('id',$slider->id)!!}
-                {!! Form::text('titre',$slider->titre)!!}
+                <div class="form-group">
+                    {!! Form::label('titre','Titre') !!}
+                    {!! Form::text('titre',$slider->titre,['class' => 'form-control'])!!}
+                </div>
+                <div class="form-control">
                 @foreach($images as $image)
-                    {{$in=false}}
+                    <?php $in=false; ?>
                     @foreach($slider->images as $sliderImage)
                         @if($image->id==$sliderImage->id)
-                            {{$in=true}}
+                                <?php $in=true; ?>
                         @endif
                     @endforeach
                     @if($in==true)
-                        {!! Form::checkbox('images[]',$image->id,true)!!}
+                        {!! Form::checkbox('images[]', $image->id ,true) !!} <img class="imgform" src="{{ URL::to('/') }}/images/{{ $image->file }}">
                     @else
-                        {!! Form::checkbox('images[]',$image->id)!!}
+                        {!! Form::checkbox('images[]', $image->id ,false) !!} <img class="imgform" src="{{ URL::to('/') }}/images/{{ $image->file }}">
                     @endif
                 @endforeach
-                {!! Form::submit() !!}
+                </div>
+                {!! Form::submit('Enregistrer',['class' => 'btn btn-primary']) !!}
                 {!! Form::close() !!}
     </div>
 @endsection
